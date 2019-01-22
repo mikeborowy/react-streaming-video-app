8fb1f43db3b6fef9cb5cc2d9e81ed423161347f9
mikeborowy <mikeborowy@gmail.com>
2019-01-22 23:26:01 +0100

    Added renderList

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,16 +8,43 @@ class StreamShow extends Component {
         this.props.onGetStreamListAPI();
     }
 
+    renderList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
         return (
             <div>
-                StreamShow
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderList()}
+                </div>
             </div>
         );
     }
 };
 
+const mapStateToProps = (state) => ({
+    streams: Object.values(state.streams)
+});
+
+const mapDisptchToProps = (dispatch) => ({
+    onGetStreamListAPI: () => dispatch(onGetStreamListAPI())
+});
+
 export default connect(
-    null,
-    { onGetStreamListAPI }
+    mapStateToProps,
+    mapDisptchToProps
 )(StreamShow);
